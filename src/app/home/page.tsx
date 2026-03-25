badbc584d8d5f72f3841a0ef476b89777c529447
valentaditya <valent.aditya1210@gmail.com>
2026-03-25 20:18:39 +0700

    pertama

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from "react";
+import Game1 from "../../components/Game1";
+
+export default function Home() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen min-w-full">
+            <h1 className="text-4xl font-bold mb-4 text-white">Selamat Datang!</h1>
+            <Game1 />
+        </div>
+    );
+}
